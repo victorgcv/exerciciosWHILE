4b68d27008195c912eb3456520df6a34b65d5bfc
Victor Gonçalves <94809017+victorgcv@users.noreply.github.com>
2025-09-17 08:28:27 -0300

    Create exercicio07.c
    7 - Faça um algoritmo usando o comando while para Imprimir os quadrados dos números de 1 a 10

---
+++ exercicio07.c
@@ -0,0 +1,25 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+7 - Faça um algoritmo usando o comando while para Imprimir os quadrados dos números de 1 a 10
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    
+    int i = 1;
+    while (i <= 10) { 
+        printf("o quadradado de %d é %d\n", i, i * i);
+        i++;
+        
+        
+    }
+
+    return 0;
+}
